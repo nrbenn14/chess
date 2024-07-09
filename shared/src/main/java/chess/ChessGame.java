e20daea9fc89c2d770615d52ce74c4968bc658e9
nrbenn14 <nrbenn14@byu.edu>
2024-07-09 14:05:36 -0600

    did stalemate, DEFINITELY needs testing. Also did checkMate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -95,7 +95,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return (isInCheck(teamColor) && isInStalemate(teamColor));
     }
 
     /**
@@ -106,7 +106,17 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                ChessPosition position = new ChessPosition(i,j);
+                if ((board.getPiece(position) != null) && (board.getPiece(position).getTeamColor() != teamColor)) {
+                    HashSet<ChessMove> potentialMoves = (HashSet<ChessMove>) board.getPiece(position).pieceMoves(board, position);
+
+                    return potentialMoves.isEmpty();
+                }
+            }
+        }
+        return false;
     }
 
     /**
